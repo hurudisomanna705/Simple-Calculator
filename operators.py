d487945bf2059026fddc05a086b77d4e1f0f1e57
hurudisomanna705 <hurudisomanna705@gmail.com>
2024-04-02 15:04:53 +0530

    claculator_4

---
+++ operators.py
@@ -0,0 +1,19 @@
+#operators
+def add(a,b):
+  A=a+b;
+  print(A)
+def sub(a,b):
+  B=a-b;
+  print(B)
+def mult(a,b):
+  C=a*b;
+  print(C)
+def div(a,b):
+  D=a/b;
+  print(D)
+def rem(a,b):
+  E=a%b;
+  print(E)
+def exp(a,b):
+  G=a**b;
+  print(G)
